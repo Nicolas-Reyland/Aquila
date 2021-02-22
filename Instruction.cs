a91dbaf26685e0be0b45f727cb5ae478b6407fd2
Nicolas Reyland <43726409+Nicolas-Reyland@users.noreply.github.com>
2021-02-22 23:45:27 +0100

    initial commit

---
+++ Instruction.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+// ReSharper disable SuggestVarOrType_SimpleTypes
+
+namespace Parser
+{
+    public abstract class Instruction
+    {
+#pragma warning disable 649
+        private bool _graphical_instruction; // #pragma : stop compiler warning due to unused _graphical_instruction variable
+#pragma warning restore 649
+
+        // getters
+        public bool isGraphical() => _graphical_instruction;
+
+        // methods
+        public abstract void execute();
+    }
+
+    public abstract class NestedInstruction : Instruction // ex: for, while, if, etc.
+    {
+        // attributes
+        protected int depth;
+        protected List<Instruction> instructions;
+
+        public override void execute()
+        {
+            foreach (Instruction instr in instructions)
+            {
+                instr.execute();
+            }
+        }
+    }
+
+    public abstract class Loop : NestedInstruction
+    {
+        private readonly Expression _condition;
+        private bool _in_loop = false;
+
+        protected Loop(Expression condition, List<Instruction> instructions)
+        {
+            this._condition = condition;
+            this.instructions = instructions;
+        }
+
+        private bool test()
+        {
+            return ((BooleanVar) _condition.evaluate()).bool_value;
+        }
+
+        public abstract void next();
+    }
+
+    public class WhileLoop : Loop
+    {
+        public WhileLoop(Expression condition, List<Instruction> instructions) : base(condition, instructions)
+        {
+            //
+        }
+
+        public override void next()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    
+    public class ForLoop : Loop
+    {
+        private readonly Instruction _start;
+        private readonly Instruction _step;
+        private Expression _condition;
+
+        public ForLoop(Instruction start, Expression condition, Instruction step,
+            List<Instruction> instructions) : base(condition, instructions)
+        {
+            this._start = start;
+            this._condition = condition;
+            this._step = step;
+            this.instructions = instructions;
+        }
+
+        public void initialize()
+        {
+            this._start.execute();
+        }
+
+        public override void execute()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void next()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class IfCondition : NestedInstruction
+    {
+        private readonly Expression _condition;
+        private readonly List<Instruction> _else_instructions;
+
+        public IfCondition(Expression condition, List<Instruction> instructions, List<Instruction> else_instructions = null)
+        {
+            this._condition = condition;
+            this.instructions = instructions;
+            this._else_instructions = else_instructions;
+        }
+
+        public override void execute()
+        {
+            if (((BooleanVar) _condition.evaluate()).bool_value)
+            {
+                foreach (Instruction instr in instructions)
+                {
+                    instr.execute();
+                }
+            }
+            else
+            {
+                foreach (Instruction instr in _else_instructions)
+                {
+                    instr.execute();
+                }
+            }
+        }
+    }
+
+    public class Declaration : Instruction
+    {
+        private readonly string _var_name;
+        private readonly Variable _var_value;
+        
+        public Declaration(string var_name, Variable var_value)
+        {
+            this._var_name = var_name;
+            this._var_value = var_value;
+            // add variable to dictionary
+            Debugging.assert(!Global.variables.ContainsKey(var_name));
+            Global.variables.Add(var_name, var_value);
+        }
+        
+        public override void execute()
+        {
+            // already in dictionary
+            Global.variables[_var_name].setValue( _var_value );
+            Debugging.assert(Global.variables[_var_name].assigned);
+        }
+    }
+
+    public class Assignment : Instruction
+    {
+        private readonly string _var_name;
+        private readonly Expression _var_value;
+
+        public Assignment(string var_name, Expression var_value) // doesn't work with "var l[0]"
+        {
+            _var_name = var_name;
+            _var_value = var_value;
+            //Debugging.assert(Global.variables.ContainsKey(_var_name)); // var l[var i] ???
+            try
+            {
+                Expression.parse(var_name);
+            }
+            catch (Exception e)
+            {
+                Debugging.print("one of the variables in the assignment doesn't exist");
+                throw Global.aquilaError();
+            }
+            Expression.variableFromName(_var_name).setName(_var_name);
+        }
+
+        public override void execute()
+        {
+            //! HERE PLS DO var l[var i] :)
+            throw new NotImplementedException("var l[var i] not done. _var_name is \"var x\" (with the var prefix)");
+            Debugging.print("executing assignment to ", _var_name);
+            Expression.variableFromName(_var_name).setValue( _var_value.evaluate() );
+        }
+    }
+    
+    public class VoidFunctionCall : Instruction
+    {
+        private readonly Expression _function;
+        private bool _called;
+
+        public VoidFunctionCall(Expression function)
+        {
+            _function = function;
+        }
+        
+        public override void execute()
+        {
+            _called = true;
+            _function.evaluate();
+        }
+
+        public bool hasBeenCalled() => _called;
+    }
+}
---
+++ Variable.cs
@@ -0,0 +1,278 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+// ReSharper disable SuggestVarOrType_SimpleTypes
+
+namespace Parser
+{
+    /// <summary>
+    /// This is a summary
+    /// <para>this is a new paragraph (new line, etc)</para>
+    /// This links to the class Expression : <see cref="Expression"/>
+    /// </summary>
+    public abstract class Variable
+    {
+        // attributes
+        private string _name;
+        public bool assigned;
+
+        // getters
+        public string getName() => _name;
+        
+        // setters
+        public void setName(string new_name) => _name = new_name;
+
+        // methods
+        public abstract dynamic getValue();
+
+        public abstract void setValue(Variable other_value);
+
+        protected void assign() => assigned = true;
+
+        protected void assertAssignment()
+        {
+            if (!assigned) throw Global.aquilaError();
+        }
+    }
+
+    public class BooleanVar : Variable
+    {
+        public bool bool_value;
+        
+        public BooleanVar(bool bool_value)
+        {
+            assigned = true;
+            this.bool_value = bool_value;
+        }
+
+        public BooleanVar not()
+        {
+            assertAssignment();
+            return new BooleanVar(!bool_value);
+        }
+
+        public BooleanVar or(BooleanVar other)
+        {
+            assertAssignment();
+            return new BooleanVar(bool_value || other.getValue());
+        }
+        
+        public BooleanVar and(BooleanVar other)
+        {
+            assertAssignment();
+            return new BooleanVar(bool_value && other.getValue());
+        }
+        
+        public BooleanVar xor(BooleanVar other)
+        {
+            assertAssignment();
+            return new BooleanVar(bool_value ^ other.getValue());
+        }
+        
+        public override dynamic getValue() => assigned ? bool_value : throw Global.aquilaError();
+
+        public override void setValue(Variable other_value)
+        {
+            bool_value = other_value.getValue();
+            if (!assigned) assign();
+        }
+    }
+
+    public class Integer : Variable
+    {
+        private int _int_value;
+        
+        public Integer(int val)
+        {
+            assigned = true;
+            _int_value = val;
+        }
+
+        public override dynamic getValue() => assigned ? _int_value : throw Global.aquilaError();
+
+        public Integer modulo(Integer other)
+        {
+            assertAssignment();
+            int int_result = _int_value % other.getValue();
+            return new Integer(int_result);
+        }
+
+        public Integer addition(Integer other)
+        {
+            assertAssignment();
+            return new Integer(this._int_value + other.getValue());
+        }
+        
+        public Integer subtraction(Integer other)
+        {
+            assertAssignment();
+            return new Integer(this._int_value - other.getValue());
+        }
+
+        public Integer division(Integer other)
+        {
+            assertAssignment();
+            return new Integer(this._int_value / other.getValue());
+        }
+
+        public Integer mult(Integer other)
+        {
+            assertAssignment();
+            return new Integer(this._int_value * other.getValue());
+        }
+
+        public int compare(Integer n1)
+        {
+            assertAssignment();
+            if (_int_value > n1.getValue())
+            {
+                return 1;
+            }
+
+            if (n1.getValue() == _int_value)
+            {
+                return 0;
+            }
+
+            return -1;
+        }
+
+        /* addition
+         * soustraction (addition mais avec l'opposé)
+         * mutliplication
+         * division
+         * modulo
+         * compariason élémentaire -> renvoie -1, 0 ou 1
+         * comparaisons annexes (<, ==, >, <=, >=)
+        */
+
+        // Integer x = new Integer(5);
+        // Integer y = new Integer(2);
+        // Integer z = x.Modulo(y);
+
+        public override void setValue(Variable other_value)
+        {
+            _int_value = other_value.getValue();
+            if (!assigned) assign();
+        }
+
+        public override string ToString()
+        {
+            return _int_value.ToString();
+        }
+    }
+
+    public class DynamicList : Variable
+    {
+        private List<Variable> _list;
+
+        public DynamicList(List<Variable> values = null)
+        {
+            if (values == null)
+            {
+                assigned = false;
+                _list = new List<Variable>();
+            }
+            else
+            {
+                assigned = true;
+                _list = values;
+            }
+        }
+        
+        public override dynamic getValue() => _list;
+        public Integer length() => assigned ? new Integer(_list.Count) : throw Global.aquilaError();
+
+        public void validateIndex(Integer index)
+        {
+            assertAssignment();
+            Debugging.assert(index.getValue() < _list.Count);
+        }
+
+        public Variable atIndex(Integer index)
+        {
+            assertAssignment();
+            return Enumerable.ElementAt(_list, index.getValue());
+        }
+
+        public void addValue(Variable x)
+        {
+            _list.Add(x);
+            if (!assigned) assign();
+        }
+        
+        public void insertValue(Variable x, Integer index)
+        {
+            assertAssignment();
+            _list.Insert(index.getValue(), x);
+        }
+        
+        public void removeValue(Integer index)
+        {
+            assertAssignment();
+            int i = index.getValue();
+            if (i < _list.Count)
+            {
+                _list.RemoveAt(i);
+            }
+            else
+            {
+                throw Global.aquilaError();
+            }
+        }
+        
+        /* insertion
+         * get item -> renvoie l'élément à l'indice
+         * set item -> list[indice] = nouvelle-valeur
+         * del item -> list.pop(indice)
+         */
+        
+        public override void setValue(Variable other_value)
+        {
+            _list = other_value.getValue();
+            if (!assigned) assign();
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            foreach (Variable variable in _list)
+            {
+                s += variable.ToString() + ", ";
+            }
+            
+            s = "[" + s.Substring(0, s.Length - 2) + "]";
+            return s;
+        }
+    }
+
+    public class FunctionCall : Variable
+    {
+        private readonly string _function_name;
+        private readonly List<Expression> _arg_expr_list;
+        private bool _called;
+        
+        public FunctionCall(string function_name, List<Expression> arg_expr_list)
+        {
+            _function_name = function_name;
+            _arg_expr_list = arg_expr_list;
+
+            assign();
+            Functions.assertFunctionExists(function_name);
+        }
+
+        public override dynamic getValue()
+        {
+            _called = true;
+            List<Variable> arg_list = _arg_expr_list.Select(x => x.evaluate()).ToList();
+            object[] args = arg_list.Select(x => (object) x).ToArray();
+            return Functions.callFunctionByName(_function_name, args).getValue();
+        }
+
+        public override void setValue(Variable other_value)
+        {
+            throw Global.aquilaError(); // should never set a value to a function call
+        }
+
+        public bool hasBeenCalled() => _called;
+    }
+}
