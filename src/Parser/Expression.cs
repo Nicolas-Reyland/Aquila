dcceb8f9268a3b2ccd1e2a1720b33238990a6492
Nicolas Reyland <nicolas.reyland@epita.fr>
2021-03-21 00:17:57 +0100

    removed unecessary dependance

--- src/Parser/Expression.cs
+++ src/Parser/Expression.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Net.Http.Headers;
 
 // ReSharper disable SuggestVarOrType_SimpleTypes
 // ReSharper disable PossibleNullReferenceException
