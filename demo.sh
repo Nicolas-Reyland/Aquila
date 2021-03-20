db288b969246eb658261f4a88162038d4c84d26e
Nicolas Reyland <43726409+Nicolas-Reyland@users.noreply.github.com>
2021-03-21 00:13:09 +0100

    basic sec & usage if $# !=

--- demo.sh
+++ demo.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+PATH=/bin
+if [ $# != 1 ]; then
+  /bin/echo "You have to pass the path to the script you want to demo as the only argument"
+  exit
+fi
 src_code=$1
-echo "Source Code of ${src_code}:"
-echo "========================================================"
-cat "${src_code}"
-echo "========================================================"
-echo "press Enter to run"
+/bin/echo "The compiled interpreter should be in the current directory. mono shoud be installed too."
+/bin/echo "Source Code of ${src_code}:"
+/bin/echo "========================================================"
+/bin/cat "${src_code}"
+/bin/echo "========================================================"
+/bin/echo "press Enter to run"
 read
-mono program.exe "${src_code}"
+/bin/mono program.exe "${src_code}"
